8b5dd483529cf2bb4daffa7e23089755b5a92e7d
Jose Cabrera <cabrera.student@gmail.com>
2021-10-14 19:54:51 +0000

    Completed Assignment 1

---
+++ assignment1/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{}
+	for i := 0; i < 11; i++  {
+		numbers = append(numbers, i)
+	}
+
+	for _, num := range(numbers) {
+		if num % 2 == 1 {
+			fmt.Println(num, "is odd")
+		} else {
+			fmt.Println(num, "is even")
+		} 
+	}
+}
